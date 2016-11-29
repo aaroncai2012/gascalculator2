b60ec819a402a75b68902ca60a92f0c31f2b5cc0
Aaron Cai <aaroncai2012@hotmail.com>
2016-11-28 18:14:09 -0800

    made structs

--- utilities.h
+++ utilities.h
@@ -4,6 +4,31 @@
 #include <vector>
 #include <string>
 
+namespace gascalculator {
 
+struct signum {
+
+  float value;
+  int sigfig;
+
+};
+
+struct element {
+
+  std::string name;
+  int amount;
+
+};
+
+struct input {
+
+  signum num;
+  std::string type;
+  std::string unit;
+  std::vector<std::string> formula;
+
+};
+
+}
 
 #endif // UTILITIES_H_
